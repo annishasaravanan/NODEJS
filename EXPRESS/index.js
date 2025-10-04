31f54dfad35b9b675076bc442709f8b44f3469c4
annishasaravanan <annishasaravanan83@gmail.com>
2025-10-04 12:57:51 +0530

    update files

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app =express();
 
+app.get("/",(req,res) =>{
+    res.status(200).send("hello express app");
+
+});
+
+app.post("/", (req, res) => {
+    res.status(200).send("post requires");
+})
 app.listen(5000, ()=>{
     console.log("server listening on pot 5000");
 })
